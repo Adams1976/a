60b9b773eebe3a74a19838cf1827d2b1923298c1
adam <adammagom00@gmail.com>
2025-05-06 01:23:56 +0300

    завершенный проект

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: './index.js', // Точка входа
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // Имя выходного файла
+        clean: true, // Очистка папки dist перед сборкой
+    },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.css$/, // Обработка CSS-файлов
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i, // Обработка изображений
+                type: 'asset/resource',
             },
         ],
     },
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'index.html')
-    }),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', // Шаблон HTML
+            filename: 'index.html',
+        }),
     ],
-    mode: 'development'
+    devServer: {
+        static: './dist',
+        open: true, // Автоматически открывать браузер
+        port: 3000, // Порт для разработки
+    },
 };
