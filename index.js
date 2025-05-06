c9bd54039f7d91c4f97e12763a3f024f228e99b9
adam <adammagom00@gmail.com>
2025-05-06 03:12:18 +0300

    git

--- index.js
+++ index.js
@@ -1,11 +1,88 @@
 import "./style.css";
+const weatherElement = document.querySelector('.weather');
+const errorElement = document.querySelector('.error');
+const apiKey = process.env.API_KEY;
 
-const apiKey = process  .env.API_KEY;
-const weatherIcon = document.querySelector('.weather-image i');
-const searchInput = document.querySelector('.search-box input');
-const searchButton = document.querySelector('.search-box button');
-const weather = document.querySelector('.weather');
-const error = document.querySelector('.error');
+// Создание HTML-структуры
+const body = document.body;
+
+const header = document.createElement('header');
+const headerText = document.createElement('p');
+headerText.textContent = 'Прогноз погоды';
+header.appendChild(headerText);
+body.appendChild(header);
+
+
+const main = document.createElement('main');
+const container = document.createElement('div');
+container.className = 'container';
+
+const searchBox = document.createElement('div');
+searchBox.className = 'search-box';
+const locationIcon = document.createElement('i');
+locationIcon.className = 'fa-solid fa-location-dot';
+const searchInput = document.createElement('input');
+searchInput.type = 'text';
+searchInput.placeholder = 'Введите название города';
+searchInput.className = 'search-input';
+const searchButton = document.createElement('button');
+searchButton.className = 'search-button fa-solid fa-magnifying-glass';
+searchBox.append(locationIcon, searchInput, searchButton);
+
+const error = document.createElement('div');
+error.className = 'error';
+error.style.display = 'none';
+const errorText = document.createElement('p');
+errorText.textContent = 'Город с таким именем не найден!';
+error.appendChild(errorText);
+
+const weather = document.createElement('div');
+weather.className = 'weather';
+weather.style.display = 'none';
+
+const weatherImage = document.createElement('div');
+weatherImage.className = 'weather-image';
+const weatherIcon = document.createElement('i');
+weatherIcon.className = 'fa-solid fa-cloud';
+weatherImage.appendChild(weatherIcon);
+
+const temp = document.createElement('h1');
+temp.className = 'temp';
+temp.innerHTML = '25 &#8451';
+
+const city = document.createElement('h2');
+city.className = 'city';
+city.textContent = 'Grozny';
+
+const details = document.createElement('div');
+details.className = 'details';
+
+const humidityCol = document.createElement('div');
+humidityCol.className = 'col';
+const humidityIcon = document.createElement('i');
+humidityIcon.className = 'fa-solid fa-water';
+const humidityText = document.createElement('div');
+humidityText.innerHTML = '<p class="humidity">50%</p><p>Влажность</p>';
+humidityCol.append(humidityIcon, humidityText);
+
+const windCol = document.createElement('div');
+windCol.className = 'col';
+const windIcon = document.createElement('i');
+windIcon.className = 'fa-solid fa-wind';
+const windText = document.createElement('div');
+windText.innerHTML = '<p class="wind">12 km/h</p><p>Скорость ветра</p>';
+windCol.append(windIcon, windText);
+
+details.append(humidityCol, windCol);
+weather.append(weatherImage, temp, city, details);
+
+
+
+container.append(searchBox, error, weather);
+main.appendChild(container);
+body.appendChild(main);
+
+
 
 // Функция для обращения к API
 async function fetchWeatherData(city) {
